b0bbbcadfaa2c0453709eae5e1f2e9ec0f738707
James Balnaves <james@balnaves.com>
2021-03-05 08:07:15 -0800

    Make SwiftyXSLT a singleton.

--- Sources/SwiftyXSLT/SwiftyXSLT.swift
+++ Sources/SwiftyXSLT/SwiftyXSLT.swift
@@ -1,7 +1,9 @@
 import Foundation
 import Wrapper
 
-public struct SwiftyXSLT {
+public class SwiftyXSLT {
+    public static let shared = SwiftyXSLT()
+    
     public func transform(xml: String, with stylesheet: String) -> String? {
         guard let result = WrapperXSLT.transformXML(xml, withStyleSheet: stylesheet) else {
             print("Unable to transform text")
